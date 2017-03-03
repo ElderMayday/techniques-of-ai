4649e476b1a72fa63181bc9001d594be3c63bb24
ElderMayday <galdarsar@gmail.com>
2017-03-03 02:56:59 +0100

    Start implementing Backward Propagation

--- src/neuralNet/Main.java
+++ src/neuralNet/Main.java
@@ -2,7 +2,8 @@ package neuralNet;
 
 public class Main {
 	public static void main(String[] args) throws Exception {
-		NeuralNet nn = new NeuralNet(2, 2, 1, new Sigmoid());
+		NeuralNet nn = new NeuralNet(2, 2, 1, new Sigmoid(), 0.05);
+		
 		nn.DoTraining(new double[]{1.0, 1.0}, new double[]{-1.0});
 	}
 }
--- src/neuralNet/NeuralNet.java
+++ src/neuralNet/NeuralNet.java
@@ -1,21 +1,23 @@
 package neuralNet;
 
+import java.util.Arrays;
 import java.util.Random;
 import java.lang.*;
 
 public class NeuralNet {
 
 	private int inputNum, hiddenNum, outputNum; // number of nodes of the layers
-	private double[][] weightIH, weightHO;      // weights of input-hidden and hidden-output layers
+	private double[][] weightIH, weightHO, newWeightIH, newWeightHO;  // weights of input-hidden and hidden-output layers
 	private IActivationFunction af;             // activation function
 	private double[] input, hiddenSum, hiddenOut, outputSum, output; // nodes values
+	private double learningRate; // derivative subtraction coefficient
 	
-	
-  	public NeuralNet(int inputNum, int hiddenNum, int outputNum, IActivationFunction af) throws Exception {
+  	public NeuralNet(int inputNum, int hiddenNum, int outputNum, IActivationFunction af, double learningRate) throws Exception {
   		this.inputNum = inputNum;
   		this.hiddenNum = hiddenNum;
   		this.outputNum = outputNum;
   		this.af = af;
+  		this.learningRate = learningRate;
   		
   		input = new double[inputNum+1];
   		input[0] = 1.0;
@@ -29,27 +31,35 @@ public class NeuralNet {
   		output = new double[outputNum];
   		
   		weightIH = new double[inputNum+1][hiddenNum];
+  		newWeightIH = new double[inputNum+1][hiddenNum];
   		for (int i = 0; i <= inputNum; i++)
   			for (int j = 0; j < hiddenNum; j++)
   				weightIH[i][j] = this.RandomWeight();
   		
   		
   		weightHO = new double[hiddenNum+1][outputNum];
+  		newWeightHO = new double[hiddenNum+1][outputNum];
   		for (int i = 0; i <= hiddenNum; i++)
   			for (int j = 0; j < outputNum; j++)
   				weightHO[i][j] = this.RandomWeight();
     }
   
   
-    public void DoTraining(double[] inputValues, double[] outputValues) throws Exception {    	
-    	if ((inputValues.length != inputNum) || (outputValues.length != outputNum))
+    public void DoTraining(double[] inputValues, double[] expectedValues) throws Exception {    	
+    	if ((inputValues.length != inputNum) || (expectedValues.length != outputNum))
     		throw new Exception("Input/output size mismatch");
     	
       	SetInputNeurons(inputValues);     // set input neurons according to input data
       
   		DoForwardPropagation();
-      	DoBackwardPropagation();
+      	DoBackwardPropagation(expectedValues);
   	}
+    
+    public double[] Calculate(double[] inputValues) {
+    	SetInputNeurons(inputValues);
+    	DoForwardPropagation();
+    	return output;
+    }
   
 	protected void SetInputNeurons(double[] inputValues) {
 		for (int i = 0; i < inputNum; i++)
@@ -80,8 +90,23 @@ public class NeuralNet {
     	}
 	}
   
-	protected void DoBackwardPropagation() {
+	protected void DoBackwardPropagation(double[] expectedValues) {		
+		// Propagate from output to hidden
+    	for (int i = 0; i < hiddenNum; i++)
+    		for (int j = 0; j < outputNum; j++)
+    			newWeightHO[i][j] = weightHO[i][j] + learningRate * (expectedValues[j] - output[j]) * this.af.Derivative(hiddenSum[i+1]) * hiddenOut[i+1];
+	
+    	// Propagate from hidden to input
+    	// TO-DO    	
     	
+    	// Swap new and old weight arrays
+    	double[][] exchange = weightIH;
+    	weightIH = newWeightIH;
+    	newWeightIH = exchange;
+    	
+    	exchange = weightHO;
+    	weightHO = newWeightHO;
+    	newWeightHO = exchange;
 	}
 
 

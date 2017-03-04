f9ff988d9e974c40c5151b2d8a69fce4cb64282d
ElderMayday <galdarsar@gmail.com>
2017-03-04 19:49:36 +0100

    Redo NeuralNet constructor

--- src/neuralNet/NeuralNet.java
+++ src/neuralNet/NeuralNet.java
@@ -5,62 +5,89 @@ import java.lang.*;
 
 public class NeuralNet {
 
-  	// node[i][j]
-	protected double[][] node; // the output value of the j-th node of the i-th layer
-      
-  	// weight[i][j][k] -- weight[layer][neuron-position left][neuron-position right]
-	protected double[][][] weight; // the synopsys from j-th node of i-th layer to k-th node of (i+1)-th layer
+    protected int layerNum;   // number of layers (incl. input and output)
+    protected int[] layerSize;  // sizes of layers
 
-	protected IActivationFunction af;
+	protected double[][] out; // out[i][j] -- the output value of the j-th node of the i-th layer
+    protected double[][] net; // net[i][j] -- the input (sum) value of j-th node of the i-th layer (for 0 layer does not matter)
+
+	protected double[][][] weight;   // weight[i][j][k] -- weight[left layer][neuron-position left][neuron-position right]
+    protected double[][][] newWeight; // newWeight[i][j][k] -- weight[left layer][neuron-position left][neuron-position right]
+
+    protected double[][] bias; // bias[i][j] -- bias from i-th layer to i+1-th layer, j-th node
+
+	protected IActivationFunction af; // arbitary activation function
+
 
 
 
   	public NeuralNet(int[] layerSize, IActivationFunction af) throws Exception {
-  		
   		if (layerSize.length < 2)
   			throw new Exception("Cannot have less than 2 layers");
   		
   		this.af = af;
+  		this.layerNum = layerSize.length;
+  		this.layerSize = layerSize;
   		
-  		node = new double[layerSize.length][];        // allocates memory for layer array
-    	for (int i = 0; i < layerSize.length; i++)  
-        	node[i] = new double[layerSize[i]];
+  		out = new double[layerNum][];        // allocates memory for node input, output arrays
+        net = new double[layerNum][];
+    	for (int i = 0; i < layerSize.length; i++) {
+            out[i] = new double[layerSize[i]];
+            net[i] = out[i] = new double[layerSize[i]];
+        }
       
-    	weight = new double[layerSize.length-1][][];
-    	for (int i = 0; i < layerSize.length-1; i++)   // allocates memory for weight array
-          	weight[i] = new double[layerSize[i]][layerSize[i+1]];      
-      	
-      	for (int i = 0; i < weight.length; i++)          // allocate random weight to each synopsys
-          	for (int j = 0; j < weight[i].length; j++)
-              	for (int k = 0; k < weight[i][j].length; k++)
-              		weight[i][j][k] = this.RandomWeight();
+    	weight = new double[layerNum-1][][];      // allocates memory for weight array
+        newWeight = new double[layerNum-1][][];
+    	for (int i = 0; i < layerNum-1; i++) {
+            weight[i] = new double[layerSize[i]][layerSize[i + 1]];
+            for (int j = 0; j < weight[i].length; j++)
+                for (int k = 0; k < weight[i][j].length; k++)
+                    weight[i][j][k] = randomWeight();
+
+            newWeight[i] = new double[layerSize[i]][layerSize[i + 1]];
+        }
+
+        bias = new double[layerNum-1][];              // allocates memory for the bias nodes weights
+    	for (int i = 0; i < layerNum-1; i++) {
+    	    bias[i] = new double[layerSize[i+1]];
+
+    	    for (int j = 0; j < layerSize[i+1]; j++)
+    	        bias[i][j] = randomWeight();
+        }
     }
 
     public void DoTraining(double[] input, double[] output) {
-      	
-      	SetInputNeurons(input);     // set input neurons according to input data
+      	setInputNeurons(input);     // set input neurons according to input data
       
-  		DoForwardPropagation();
-      	DoBackwardPropagation();
+  		doForwardPropagation();
+      	doBackwardPropagation();
   	}
 
+  	public double[] GetOutput(double[] input) {
+  	    setInputNeurons(input);
+  	    doForwardPropagation();
 
-  
-	protected void SetInputNeurons(double[] values) {
+  	    return null; // TO-FINISH
+    }
+
+
+
+
+	protected void setInputNeurons(double[] values) {
       	for (int i = 0; i < values.length; i++) {
-      		node[0][i] = values[i];
+      		out[0][i] = values[i];
       	}
 	}
 
-	protected void DoForwardPropagation() {
+	protected void doForwardPropagation() {
     	
 	}
   
-	protected void DoBackwardPropagation() {
+	protected void doBackwardPropagation() {
     	
 	}
 
-	protected double RandomWeight() {
+	protected double randomWeight() {
 		Random rand = new Random();
 		return rand.nextDouble();
 	}
